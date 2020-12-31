1be9accecd2fe9762274a25206573b62e8024348
rainswerld <m.rainrobinson@gmail.com>
2020-12-31 16:11:45 -0700

    Add ratings to Whiskey model

--- angels-share/models/whiskey.js
+++ angels-share/models/whiskey.js
@@ -8,6 +8,10 @@ const whiskeySchema = new mongoose.Schema({
   notes: {
     type: String,
   },
+  rating: {
+    type: Number,
+    enum: [1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 4.25, 4.5, 4.75, 5]
+  },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
