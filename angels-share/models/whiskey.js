869d1b414b687f3dfdddb345b9f6dc71250d443e
rainswerld <m.rainrobinson@gmail.com>
2020-12-31 16:19:22 -0700

    Update rating in Whiskey schema without enum

--- angels-share/models/whiskey.js
+++ angels-share/models/whiskey.js
@@ -9,8 +9,7 @@ const whiskeySchema = new mongoose.Schema({
     type: String,
   },
   rating: {
-    type: Number,
-    enum: [1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 4.25, 4.5, 4.75, 5]
+    type: Number
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
